b09d890828aadce5ce823410590551cf3cb5170e
macbook <graceolubiyi@gmail.com>
2026-07-13 12:54:12 +0100

    github profile

---
+++ src/main/java/com/olubiyigrace/OlubiyigraceApplication.java
@@ -0,0 +1,13 @@
+package com.olubiyigrace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlubiyigraceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OlubiyigraceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/olubiyigrace/OlubiyigraceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olubiyigrace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlubiyigraceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
